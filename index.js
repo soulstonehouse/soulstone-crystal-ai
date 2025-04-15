418f813a3334aa831386c3e49c6d1503ea704d0a
soulstonehouse <13064383@qq.com>
2025-04-15 16:24:53 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-const { OpenAIApi, Configuration } = require('openai');
-
 export default async function handler(req, res) {
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Method not allowed' });
